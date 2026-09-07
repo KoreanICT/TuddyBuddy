0274c58c253e6d9b4a0f11c58fd444c5ef2f767a
jyh928578@gmail.com <jyh928578@gmail.com>
2026-09-07 18:22:09 +0900

    병합문제 해결

--- front/src/router/AppRoutes.tsx
+++ front/src/router/AppRoutes.tsx
@@ -4,16 +4,11 @@ import { ProductSelector } from "../cont/point/ProductSelector";
 import PerformanceAnalytics from "../cont/statistics/PerformanceAnalytics";
 import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-
-import Home from "../cont/Home";
 import VideoSummary from "../cont/video/VideoSummary";
-import {Group_Home} from "../cont/study_group/Group_Home";
+import { Group_Home } from "../cont/study_group/Group_Home";
 import Group_Create from "../cont/study_group/Group_Create";
 import { Group_Detail } from "../cont/study_group/Group_Detail";
 import AdminHome from "../cont/admin/AdminHome";
-
-import Layout from "../layout/Layout";
-import AdminLayout from "../layout/admin/AdminLayout";
 import MemberManagement from "../cont/admin/member/MemberManagement";
 import BoardManagement from "../cont/admin/board/BoardManagement";
 import StudentCode from "../cont/friend/StudentCode";
@@ -22,6 +17,9 @@ import FriendList from "../cont/friend/FriendList";
 import ReportList from "../cont/admin/report/ReportList";
 import ReportDetail from "../cont/admin/report/ReportDetail";
 import ReportReply from "../cont/admin/report/ReportReply";
+
+import Layout from "../layout/Layout";
+import AdminLayout from "../layout/admin/AdminLayout";
 
 import SelfStudy from "../cont/SelfStudy";
 interface RouteItem {
@@ -42,15 +40,18 @@ const AppRoutes: React.FC = () => {
 
 
         { path: '/createGroup', element: <Group_Create /> },
-        { path: '/homeGroup', element: <Group_Home/> },
-        { path: '/group/detail', element: <Group_Detail/> },
-      
-      
+        { path: '/homeGroup', element: <Group_Home /> },
+        { path: '/group/detail', element: <Group_Detail /> },
+
+
         // common
         { path: "/", element: <Home /> },
         { path: "/friend/code", element: <StudentCode /> },
         { path: "/friend/add", element: <FriendAdd /> },
         { path: "/friend/list", element: <FriendList /> },
+        { path: '/point', element: <ProductSelector /> },
+        { path: '/statistics', element: <PerformanceAnalytics /> },
+        { path: '/selfStudy', element: <SelfStudy /> },
 
 
         // admin
@@ -61,24 +62,31 @@ const AppRoutes: React.FC = () => {
         { path: "/admin/reportDetail/:id", element: <ReportDetail /> },
         { path: "/admin/reportReply/:id", element: <ReportReply /> },
 
-
         // user? auth? account?
     ];
 
-        { path: '/point', element: <ProductSelector/> },
-        { path: '/statistics', element: <PerformanceAnalytics/> }
-        { path: '/selfStudy', element: <SelfStudy /> },
-    ]
-    return (
-        <Routes>
-            {
-                routeList.map((route, idx) => (
-                    <Route key={idx} {...route} />
-                ))
-            }
-            
-        </Routes>
-    )
-}
+
+return (
+        <>
+            {isManage ? (
+                <AdminLayout>
+                    <Routes>
+                        {routeList.map((route, idx) => (
+                            <Route key={idx} {...route} />
+                        ))}
+                    </Routes>
+                </AdminLayout>
+            ) : (
+                <Layout>
+                    <Routes>
+                        {routeList.map((route, idx) => (
+                            <Route key={idx} {...route} />
+                        ))}
+                    </Routes>
+                </Layout>
+            )}
+        </>
+    );
+};
 
 export default AppRoutes;
